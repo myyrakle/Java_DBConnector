4ae52f11056cf27ac949443b64314d1e0a0cd12e
myyrakle <sssang97@naver.com>
2019-12-27 18:48:21 +0900

    Update MongoDBConnector.java

--- src/MongoDBConnector.java
+++ src/MongoDBConnector.java
@@ -1,22 +1,35 @@
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
 
 public class MongoDBConnector implements DBConnector
 {
+    private final static String DB_NAME = "";
     private MongoClient mongoClient;
+    private MongoDatabase db;
 
     @Override public void connect()
     {
         mongoClient = new MongoClient();
+        db = mongoClient.getDatabase(DB_NAME);
     }
 
     @Override public void disconnect()
     {
         mongoClient.close();
         mongoClient = null;
+        db = null;
     }
 
     @Override public boolean isConnected()
     {
         return mongoClient != null;
+    }
+
+    public MongoCollection<Document> getCollection(String collectionName)
+    {
+        return db.getCollection(collectionName);
     }
 }
